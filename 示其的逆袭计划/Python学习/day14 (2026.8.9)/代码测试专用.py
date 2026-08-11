9244593fa2c897082e65680a9c2f88f935474ce7
WangZiqi0115 <wangziqi511080@163.com>
2026-08-11 16:57:28 +0800

    Day16高级解包完成：更新README、学习日志与50天计划

--- 示其的逆袭计划/Python学习/day14 (2026.8.9)/代码测试专用.py
+++ 示其的逆袭计划/Python学习/day14 (2026.8.9)/代码测试专用.py
@@ -12,10 +12,12 @@
 # # 示例：浮点误差 1010 1001       
 #                 0010 1000 1001 
 # print(0.1 + 0.2)
-nums = [11,2,3,2,3,4,4,5,5]
-num = nums[0]
-for i in range(1,len(nums)):
-    num = num^nums[i]
-    print(num)
+# nums = [11,2,3,2,3,4,4,5,5]
+# num = nums[0]
+# for i in range(1,len(nums)):
+#     num = num^nums[i]
+#     print(num)
+s = "123"
+print(s[::-1])
     
 
